5cce019013ce1738e9036b32d66d299d8132e08c
leedaehee <vckfnv@naver.com>
2021-09-30 12:17:15 +0000

    add roberta case in inference

--- inference.py
+++ inference.py
@@ -36,6 +36,32 @@ def inference(model, tokenized_sent, device):
   
   return np.concatenate(output_pred).tolist(), np.concatenate(output_prob, axis=0).tolist()
 
+def inference_roberta(model, tokenized_sent, device):
+  """
+    test dataset을 DataLoader로 만들어 준 후,
+    batch_size로 나눠 model이 예측 합니다.
+  """
+  dataloader = DataLoader(tokenized_sent, batch_size=16, shuffle=False)
+  model.eval()
+  output_pred = []
+  output_prob = []
+  for i, data in enumerate(tqdm(dataloader)):
+    with torch.no_grad():
+      outputs = model(
+          input_ids=data['input_ids'].to(device),
+          attention_mask=data['attention_mask'].to(device)
+          )
+    logits = outputs[0]
+    prob = F.softmax(logits, dim=-1).detach().cpu().numpy()
+    logits = logits.detach().cpu().numpy()
+    result = np.argmax(logits, axis=-1)
+
+    output_pred.append(result)
+    output_prob.append(prob)
+  
+  return np.concatenate(output_pred).tolist(), np.concatenate(output_prob, axis=0).tolist()
+
+
 def num_to_label(label):
   """
     숫자로 되어 있던 class를 원본 문자열 라벨로 변환 합니다.
@@ -80,7 +106,10 @@ def main(args):
   Re_test_dataset = RE_Dataset(test_dataset ,test_label)
 
   ## predict answer
-  pred_answer, output_prob = inference(model, Re_test_dataset, device) # model에서 class 추론
+  if 'roberta' in Tokenizer_NAME:
+    pred_answer, output_prob = inference_roberta(model, Re_test_dataset, device) # model에서 class 추론
+  else:  
+    pred_answer, output_prob = inference(model, Re_test_dataset, device) # model에서 class 추론
   pred_answer = num_to_label(pred_answer) # 숫자로 된 class를 원래 문자열 라벨로 변환.
   
   ## make csv file with predicted answer
