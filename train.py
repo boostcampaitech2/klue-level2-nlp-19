7a828934266a649068129ec5805cbaca9cfbc65e
JunsooLee <junsoolee65@naver.com>
2021-10-02 04:46:54 +0000

    Change Some

--- train.py
+++ train.py
@@ -68,7 +68,7 @@ def label_to_num(label):
 
 def train(args):
   # load model and tokenizer
-  MODEL_NAME = "monologg/koelectra-base-v3-discriminator"
+  MODEL_NAME = 'roberta-large'
   # MODEL_NAME = "klue/roberta-base"
   tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
 
@@ -82,7 +82,7 @@ def train(args):
 
   # 기본 데이터셋에 Augmentation된 내용 추가
   train_data = train_data.append(add_train, ignore_index=True)
-  # dev_dataset = dev_dataset.append(add_dev, ignore_index=True)
+  dev_dataset = dev_dataset.append(add_dev, ignore_index=True)
 
   train_label = label_to_num(train_dataset['label'].values)
   dev_label = label_to_num(dev_dataset['label'].values)
@@ -148,7 +148,7 @@ if __name__ == '__main__':
   parser = argparse.ArgumentParser()
 
   parser.add_argument('--epoch', type=int, default=10, help='number of epochs to train (default: 10)')
-  parser.add_argument('--batch_size', type=int, default=64, help='size of batchs to train (default: 64)')
+  parser.add_argument('--batch_size', type=int, default=32, help='size of batchs to train (default: 64)')
   parser.add_argument('--weight_decay', type=float, default=0.01, help='weight decay to train (default: 0.01)')
   parser.add_argument('--learning_rate', type=float, default=0.00001, help='learning rate to train (default: 1e-5)')
   args = parser.parse_args()
